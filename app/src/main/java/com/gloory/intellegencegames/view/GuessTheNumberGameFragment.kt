67347322f4c0ca4bc3cee257410c1a7a6dcd4163
Hilal <hilaltokgoz98@gmail.com>
2024-03-07 14:31:14 +0300

    guess the number game arranged

--- app/src/main/java/com/gloory/intellegencegames/view/GuessTheNumberGameFragment.kt
+++ app/src/main/java/com/gloory/intellegencegames/view/GuessTheNumberGameFragment.kt
@@ -11,12 +11,12 @@ import com.gloory.intellegencegames.R
 import com.gloory.intellegencegames.databinding.DifficultyScreenDialogBinding
 import com.gloory.intellegencegames.databinding.FragmentGuessTheNumberGameBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import kotlin.random.Random
 
 
 class GuessTheNumberGameFragment : Fragment() {
     private var _binding: FragmentGuessTheNumberGameBinding? = null
     private val binding get() = _binding!!
+    var right=4
 
     var realNumberList: List<Char> = listOf()
     var guessNumberList: List<Char> = listOf()
@@ -53,13 +53,13 @@ class GuessTheNumberGameFragment : Fragment() {
             mediumImage.setOnClickListener {
                 dialog.dismiss()
                 binding.guessNumberText.filters = arrayOf(InputFilter.LengthFilter(4))
-
+                digit4Process()
 
             }
             hardImage.setOnClickListener {
                 dialog.dismiss()
                 binding.guessNumberText.filters = arrayOf(InputFilter.LengthFilter(5))
-
+                digit5Process()
 
             }
         }
@@ -81,6 +81,47 @@ class GuessTheNumberGameFragment : Fragment() {
             addButton.setOnClickListener {
                 val guessNumber = binding.guessNumberText.text.toString()
                 if (guessNumber.length == 3) {
+                    binding.guessNumberText.setText("")
+                    right--
+                    guess1.text = "${guess1.text}$guessNumber\n"
+                    //guess1 al ve parçala
+                    guessNumberList = guessNumber.toCharArray().toList()
+                    var hint = ""
+
+                    for ((index, value) in realNumberList.withIndex()) {
+                        if (guessNumberList.contains(value)) {  // tahmin edilen rakam gerçek listede var
+                            if (guessNumberList[index] == value) {    //hem indeks hem rakam var
+
+                                hint += "+"
+                            } else {
+                                hint += "-"
+                            }
+                        }
+                    }
+                    //Sonuçlar basılacak
+                    hint1.text = "${hint1.text}$hint\n"
+                }
+
+            }
+        }
+       if (right==0 && binding.hint1.text != "+++"){
+          // TODO("Dialog oluştur Kaybettiniz Random= 512 yazdır)
+       }else if(binding.hint1.text == "+++" ){
+
+           // TODO("Dialog oluştur Kazandınız Random= 512 yazdır)
+        }
+    }
+
+    private fun digit4Process() {
+        //gerçek sayı parçalandı listeye kondu
+        realNumberList = randomNumber4Digits.toString().toCharArray().toList()
+
+        println(" randomDigit  $randomNumber4Digits")
+
+        binding.apply {
+            addButton.setOnClickListener {
+                val guessNumber = binding.guessNumberText.text.toString()
+                if (guessNumber.length == 4) {
                     binding.guessNumberText.setText("")
                     guess1.text = "${guess1.text}$guessNumber\n"
                     //guess1 al ve parçala
@@ -105,14 +146,47 @@ class GuessTheNumberGameFragment : Fragment() {
         }
 
     }
+    private fun digit5Process() {
+        //gerçek sayı parçalandı listeye kondu
+        realNumberList = randomNumber5Digits.toString().toCharArray().toList()
 
-    fun randomNumber( digitCount:Int): Int {
+        println(" randomDigit  $randomNumber5Digits")
+
+        binding.apply {
+            addButton.setOnClickListener {
+                val guessNumber = binding.guessNumberText.text.toString()
+                if (guessNumber.length == 5) {
+                    binding.guessNumberText.setText("")
+                    guess1.text = "${guess1.text}$guessNumber\n"
+                    //guess1 al ve parçala
+                    guessNumberList = guessNumber.toCharArray().toList()
+                    var hint = ""
+
+                    for ((index, value) in realNumberList.withIndex()) {
+                        if (guessNumberList.contains(value)) {  // tahmin edilen rakam gerçek listede var
+                            if (guessNumberList[index] == value) {    //hem indeks hem rakam var
+
+                                hint += "+"
+                            } else {
+                                hint += "-"
+                            }
+                        }
+                    }
+                    //Sonuçlar basılacak
+                    hint1.text = "${hint1.text}$hint\n"
+                }
+
+            }
+        }
+    }
+
+    fun randomNumber(digitCount: Int): Int {
         var uniqueNumber: Int
         do {
-            uniqueNumber = when(digitCount){
-                3 ->  (100..1000).random()
-                4 ->  (1000..10000).random()
-                5 ->  (10000..100000).random()
+            uniqueNumber = when (digitCount) {
+                3 -> (100..1000).random()
+                4 -> (1000..10000).random()
+                5 -> (10000..100000).random()
                 else -> return -1
             }
         } while (uniqueNumber.toString().toSet().size != digitCount)
