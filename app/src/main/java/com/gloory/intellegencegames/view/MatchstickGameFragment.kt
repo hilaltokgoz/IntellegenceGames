2ce6d42b5a8563e81051d8d97c73fa2acf07db0d
Hilal <hilaltokgoz98@gmail.com>
2024-11-05 15:59:13 +0300

    kibrit oyunu güncelleme

--- app/src/main/java/com/gloory/intellegencegames/view/MatchstickGameFragment.kt
+++ app/src/main/java/com/gloory/intellegencegames/view/MatchstickGameFragment.kt
@@ -1,13 +1,17 @@
 package com.gloory.intellegencegames.view
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.GridLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.gloory.intellegencegames.R
 import com.gloory.intellegencegames.databinding.FragmentMatchingGameBinding
 import com.gloory.intellegencegames.databinding.FragmentMatchstickGameBinding
@@ -22,6 +26,7 @@ class MatchstickGameFragment : Fragment() {
     private var userSelectedCount = 0
     private val maxSelectable = 3
     private lateinit var matchstickViews: MutableList<ImageView>
+    private var difficultyLevel: String = "normal" // Varsayılan zorluk seviyesi
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -99,44 +104,39 @@ class MatchstickGameFragment : Fragment() {
         val mediumLayout = dialogView.findViewById<LinearLayout>(R.id.mediumLayout)
         val hardLayout = dialogView.findViewById<LinearLayout>(R.id.hardLayout)
 
-        val onClickListener = { difficulty: Int ->
-            when (difficulty) {
-                0 -> {
-                    createMatchsticks(21)
-                    bottomSheetDialog.dismiss()
-                }
-
-                1 -> {
-                    createMatchsticks(35)
-                    bottomSheetDialog.dismiss()
-                }
-
-                2 -> {
-                    createMatchsticks(42)
-                    bottomSheetDialog.dismiss()
-                }
-            }
-            bottomSheetDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        easyLayout.setOnClickListener {
+            createMatchsticks(21)
             bottomSheetDialog.dismiss()
         }
-        easyLayout.setOnClickListener {
-            onClickListener(0)
-        }
         mediumLayout.setOnClickListener {
-            onClickListener(1)
+            difficultyLevel = "normal"
+            createMatchsticks(35)
+            bottomSheetDialog.dismiss()
         }
         hardLayout.setOnClickListener {
-            onClickListener(2)
+            difficultyLevel = "hard"
+            createMatchsticks(42)
+            bottomSheetDialog.dismiss()
         }
 
-        //  bottomSheetDialog.setCancelable(false)
+        bottomSheetDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
         bottomSheetDialog.show()
     }
 
 
     // Basit bir strateji: Bilgisayar kalan kibrit sayısına göre hareket eder
     private fun computerMove() {
-        val matchsticksTaken = if (totalMatchsticks <= 3) totalMatchsticks else (1..3).random()
+        val matchsticksTaken: Int = when {
+            totalMatchsticks <= 1 -> totalMatchsticks // 0 veya 1 kalırsa, hepsini al
+            totalMatchsticks == 2 -> 1 // 2 kalırsa, 1 al
+            totalMatchsticks == 3 -> 2 // 3 kalırsa, 2 al
+            totalMatchsticks % 4 == 1 -> 1 // Kullanıcının kazanmasını engellemek için 1 al
+            totalMatchsticks % 4 == 2 -> 2 // Kullanıcının kazanmasını engellemek için 2 al
+            totalMatchsticks % 4 == 3 -> 3 // Kullanıcı en fazla 3 alabilir
+
+            else -> (1..3).random() // Rastgele 1, 2 veya 3 al
+        }
+         //kibriti gizle
         for (i in 0 until matchsticksTaken) {
             if (totalMatchsticks > 0) {
                 matchstickViews[totalMatchsticks - 1].visibility = View.INVISIBLE
@@ -149,9 +149,61 @@ class MatchstickGameFragment : Fragment() {
 
     private fun checkGameStatus(player: String) {
         if (totalMatchsticks <= 0) {
-            binding.tvGameStatus.text = "$player oyunu kaybetti!"
-            binding.btnUserMove.isEnabled = false
+            if (player == "Kullanıcı") {
+                binding.tvGameStatus.text = "Kullanıcı oyunu kaybetti!"
+                binding.btnUserMove.isEnabled = false
+                showResultScreenDialog("Kullanıcı") // Kullanıcının kaybettiği bilgisini geçir
+            } else {
+                binding.tvGameStatus.text = "Bilgisayar oyunu kaybetti!"
+                binding.btnUserMove.isEnabled = false
+                showResultScreenDialog("Bilgisayar") // Bilgisayarın kaybettiği bilgisini geçir
+            }
         }
     }
+    private fun showResultScreenDialog(player: String) {
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_game_completed, null)
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+            .setCancelable(false)
 
+        val alertDialog = dialogBuilder.create()
+        alertDialog.show()
+        alertDialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        val playAgainButton = dialogView.findViewById<Button>(R.id.btn_play_again)
+        val exitButton = dialogView.findViewById<Button>(R.id.btn_exit)
+        val tvCongratulations = dialogView.findViewById<TextView>(R.id.tv_congratulations)
+        val ivCongratulations = dialogView.findViewById<ImageView>(R.id.iv_congratulations)
+        if (player == "Kullanıcı") {
+            // Kullanıcı kaybetti
+            tvCongratulations.text = "Kaybettiniz!"
+            ivCongratulations.setImageResource(R.drawable.sad)
+        } else {
+            // Kullanıcı kazandı
+            tvCongratulations.text = "Tebrikler!"
+            ivCongratulations.setImageResource(R.drawable.ic_celebration)
+        }
+        playAgainButton.setOnClickListener {
+            alertDialog.dismiss()
+            resetGame()
+            showDifficultyBottomSheetDialog()    //Zorluk seçme dialoğu göster
+        }
+
+        exitButton.setOnClickListener {
+            alertDialog.dismiss()
+            findNavController().navigate(R.id.homeFragment)
+
+        }
+    }
+    private fun resetGame() {
+        totalMatchsticks = 0
+        userSelectedCount = 0
+        binding.tvGameStatus.text = ""
+        binding.btnUserMove.isEnabled = true // Tekrar oynamak için butonu aktif hale getir
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
